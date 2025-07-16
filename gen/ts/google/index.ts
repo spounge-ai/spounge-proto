3a3f66368515be56006608d83599a83904b1178b
Evan Huang <evanhuang47@gmail.com>
2025-07-15 20:45:19 -0400

    feat: Go and TS update with buf gen, https, connect

--- gen/ts/google/index.ts
+++ gen/ts/google/index.ts
@@ -1,1 +1,6 @@
-export * from './protobuf';
+export * from './geo';
+export * from './longrunning';
+export * from './type';
+export * from './rpc';
+export * from './bytestream';
+export * from './api';
